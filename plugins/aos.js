26aa0cc617688433d57f5af9dc377d09711b56a3
Kaguwo <kaguwomin@gmail.com>
2021-01-16 22:01:25 +0300

    Update aos.js

--- plugins/aos.js
+++ plugins/aos.js
@@ -4,7 +4,7 @@ import "aos/dist/aos.css";
 export default ({ app }) => {
   app.AOS = new AOS.init({ 
     disable: 'mobile',
-    // offset: 200,
+    offset: 200,
     duration: 600,
     easing: 'ease-in-out-cubic',
     once: true
